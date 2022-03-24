76c8d5e65581e4f7e3dcbb55da8d853b4e274a2e
April Yeats <yeats.april@gmail.com>
2022-03-24 20:56:24 +0100

    use custom routes to better reflect task spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :v1 do
     resources :players, :playthroughs
-    resources :weekly_summaries, only: [:index]
     resources :impact_reports, only: [:index]
+    get "weekly_summaries/score" => "weekly_summaries#score", as: :score
+    get "weekly_summaries/time_played" => "weekly_summaries#time_played", as: :time_played
   end
 end
