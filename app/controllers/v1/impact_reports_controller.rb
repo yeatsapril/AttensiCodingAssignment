631c51ea9de560d71e688d7f94b1d3a3be3a5453
April Yeats <yeats.april@gmail.com>
2022-03-25 21:19:00 +0100

    reject Players who have no logged playthoughs from ImpactReport as it causes errors

--- app/controllers/v1/impact_reports_controller.rb
+++ app/controllers/v1/impact_reports_controller.rb
@@ -1,8 +1,8 @@
 module V1
   class ImpactReportsController < ApplicationController
     def index
-      @players = Player.all
-      render json: @players, each_serializer: V1::ImpactReportSerializer
+      players = Player.all.reject { |i| i.playthroughs.empty? }
+      render json: players, each_serializer: V1::ImpactReportSerializer
     end
   end
 end
